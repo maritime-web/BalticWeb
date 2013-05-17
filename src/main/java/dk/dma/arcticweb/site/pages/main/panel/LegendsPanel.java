ff9f33207a3d1a9cc275f90e47dfc0772c55c689
tejl <jesper@lundogbendsen.dk>
2013-05-17 15:41:22 +0200

    EMBRYO-14: Arbejde i at gøre LegendsPanel så uafhængig som muligt af resten af applikationen.

--- src/main/java/dk/dma/arcticweb/site/pages/main/panel/LegendsPanel.java
+++ src/main/java/dk/dma/arcticweb/site/pages/main/panel/LegendsPanel.java
@@ -1,32 +1,28 @@
 package dk.dma.arcticweb.site.pages.main.panel;
 
+import org.apache.wicket.markup.head.IHeaderResponse;
+import org.apache.wicket.markup.head.OnLoadHeaderItem;
 import org.apache.wicket.markup.html.panel.Panel;
 
 @SuppressWarnings("serial")
 public class LegendsPanel extends Panel {
+	
+	private final static String JS_INIT = "embryo.legendsPanel.init();";
 
 	public LegendsPanel(String id) {
 		super(id);
-		
-//		notLoggedIn = new WebMarkupContainer("not_logged_in");
-//		loggedIn = new WebMarkupContainer("logged_in");
-//		notLoggedIn.setVisible(false);
-//		loggedIn.setVisible(false);
-//				
-//		add(notLoggedIn);
-//		loggedIn.add(new Label("username", new PropertyModel<UserPanel>(this, "username")));
-//		add(loggedIn);
-//		
-//		Link<String> logoutLink = new Link<String>("logout_link") {
-//			private static final long serialVersionUID = 1L;
-//			@Override
-//			public void onClick() {
-//				LOG.info("Logging out: " + getUsername());
-//				ArcticWebSession.get().logout();
-//				setResponsePage(new FrontPage());
-//			}
-//		};
-//		loggedIn.add(logoutLink);		
+	}
+
+	@Override
+	public void renderHead(IHeaderResponse response) {
+		super.renderHead(response);
+		// if component is disabled we don't have to load the JQueryUI datepicker
+		if (!isEnabledInHierarchy())
+			return;
+		// initialize component
+		response.render(OnLoadHeaderItem.forScript(JS_INIT));
 	}
 	
+	
+
 }
