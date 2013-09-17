008ffee390e61e4f2e465499bb1539f7dda46779
tejl <jesper@lundogbendsen.dk>
2013-09-17 14:13:31 +0200

    EMBRYO-38: voyageid from URL used to prepopulate voyage input field.

--- src/main/webapp/js/routeUpload.js
+++ src/main/webapp/js/routeUpload.js
@@ -8,83 +8,91 @@
  */
 
 (function() {
-	"use strict";
+    "use strict";
 
-	var url = 'rest/routeUpload/single/';
+    var url = 'rest/routeUpload/single/';
 
-	var module = angular.module('embryo.routeUpload',['embryo.voyageService', 'ui.bootstrap', 'blueimp.fileupload']);
-	
-	module.config([ '$httpProvider', 'fileUploadProvider', function($httpProvider, fileUploadProvider) {
-		delete $httpProvider.defaults.headers.common['X-Requested-With'];
-		fileUploadProvider.defaults.redirect = window.location.href.replace(/\/[^\/]*$/, '/cors/result.html?%s');
-		if (false) {
-			// Demo settings:
-			angular.extend(fileUploadProvider.defaults, {
-				// Enable image
-				// resizing, except for
-				// Android and Opera,
-				// which actually
-				// support image
-				// resizing, but fail to
-				// send Blob objects via
-				// XHR requests:
-				disableImageResize : /Android(?!.*Chrome)|Opera/.test(window.navigator.userAgent),
-				maxFileSize : 5000000,
-				acceptFileTypes : /(\.|\/)(gif|jpe?g|png)$/i
-			});
-		}
-	} ]);
+    var module = angular.module('embryo.routeUpload', [ 'embryo.voyageService', 'ui.bootstrap', 'blueimp.fileupload' ]);
 
-	// embryo.angular.controller('embryo.RouteUploadCtrl', [ '$scope',
-	// function($scope) {
+    module.config([ '$httpProvider', 'fileUploadProvider', function($httpProvider, fileUploadProvider) {
+        delete $httpProvider.defaults.headers.common['X-Requested-With'];
+        fileUploadProvider.defaults.redirect = window.location.href.replace(/\/[^\/]*$/, '/cors/result.html?%s');
+        if (false) {
+            // Demo settings:
+            angular.extend(fileUploadProvider.defaults, {
+                // Enable image
+                // resizing, except for
+                // Android and Opera,
+                // which actually
+                // support image
+                // resizing, but fail to
+                // send Blob objects via
+                // XHR requests:
+                disableImageResize : /Android(?!.*Chrome)|Opera/.test(window.navigator.userAgent),
+                maxFileSize : 5000000,
+                acceptFileTypes : /(\.|\/)(gif|jpe?g|png)$/i
+            });
+        }
+    } ]);
 
-	embryo.RouteUploadCtrl = function($scope, $routeParams, VoyageService) {
+    // embryo.angular.controller('embryo.RouteUploadCtrl', [ '$scope',
+    // function($scope) {
 
-		if ($routeParams.mmsi) {
-			VoyageService.getVoyages($routeParams.mmsi, function(voyages) {
-				$scope.voyages = voyages;
-			});
-		}
+    embryo.RouteUploadCtrl = function($scope, $routeParams, VoyageService) {
+        var initUpload = function() {
+            var index;
+            if ($routeParams.mmsi) {
+                VoyageService.getVoyages($routeParams.mmsi, function(voyages) {
+                    $scope.voyages = voyages;
 
-		// Choosing a new file will replace the old one
-		$scope.$on('fileuploadadd', function(e, data) {
-			$scope.queue = [];
-		});
+                    if ($routeParams.voyageId) {
+                        for (index in $scope.voyages) {
+                            if ($scope.voyages[index].id === $routeParams.voyageId) {
+                                $scope.selectedVoyage = $scope.voyages[index];
+                                return;
+                            }
+                        }
+                    }
+                });
+            }
+        };
 
-		$scope.options = {
-			url : url,
-			done : function(e, data) {
-				$.each(data.result.files, function(index, file) {
-					$scope.message = "Uploaded route '" + file.name + "'";
-					$scope.uploadedFile = file;
-				});
-			}
-		};
+        initUpload();
 
-		$scope.uploaded = function() {
-			return $scope.uploadedFile !== null && typeof $scope.uploadedFile === "object";
-		};
+        // Choosing a new file will replace the old one
+        $scope.$on('fileuploadadd', function(e, data) {
+            $scope.queue = [];
+        });
 
-		$scope.reset = function() {
-			// $scope.fileUploadForm.clear(); - coming in Angular 1.1.1
-			$scope.uploadedFile = null;
-			$scope.message = null;
-			$scope.cancel();
-		};
+        $scope.options = {
+            url : url,
+            done : function(e, data) {
+                $.each(data.result.files, function(index, file) {
+                    $scope.message = "Uploaded route '" + file.name + "'";
+                    $scope.uploadedFile = file;
+                });
+            }
+        };
 
-		$scope.$on('fileuploadsubmit', function(e, data) {
-			if (typeof $scope.voyageDatum !== "undefined") {
-				data.formData = {
-					voyageId : $scope.voyageDatum.id
-				};
-			}
-		});
-//		var voyageId = $routeParams.voyageId;
-//		
-//		console.log(voyageId);
-//		console.log($('#routeUpload').find('#voyageName').length);
-//		$('#routeUpload').find('#voyageName').typeahead('setQuery', 'Miami');
-		
-	};
+        $scope.uploaded = function() {
+            return $scope.uploadedFile !== null && typeof $scope.uploadedFile === "object";
+        };
+
+        $scope.reset = function() {
+            // $scope.fileUploadForm.clear(); - coming in Angular 1.1.1
+            $scope.uploadedFile = null;
+            $scope.message = null;
+            $scope.cancel();
+            initUpload();
+        };
+
+        $scope.$on('fileuploadsubmit', function(e, data) {
+            if (typeof $scope.selectedVoyage !== "undefined") {
+                data.formData = {
+                    voyageId : $scope.selectedVoyage.id
+                };
+            }
+        });
+    };
 
 }());
