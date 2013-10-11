c114bd5a74c28f16cc9f6964fcf7f74a95ecf90f
tejl <jesper@lundogbendsen.dk>
2013-10-11 08:28:21 +0200

    added 'TEST' to MSI mock texts

--- src/main/java/dk/dma/embryo/msi/MsiClientMock.java
+++ src/main/java/dk/dma/embryo/msi/MsiClientMock.java
@@ -29,37 +29,37 @@ public class MsiClientMock implements MsiClient {
         try {
             List<MsiItem> result = new ArrayList<>();
 
-            result.add(new MsiItemMock(DATE_FORMAT.parse("2013-10-10"), "Shoal", 72.7516667, -056.2500000, "Greenland West", "Upernavik",
-                    "NAVIGATIONAL WARNING JOINT ARCTIC COMMAND NO. 014/13. " +
+            result.add(new MsiItemMock(DATE_FORMAT.parse("2013-10-10"), "Shoal (TEST)", 72.7516667, -056.2500000, "Greenland West", "Upernavik",
+                    "TEST - NAVIGATIONAL WARNING JOINT ARCTIC COMMAND NO. 014/13. " +
                             "WEST COAST OF GREENLAND, OFF UPERNAVIK CHART 1710 " +
                             "IN POSITION 7245,1 N - 05615,0W (WGS84) A 17 METER SHOAL " +
                             "HAS BEEN REPORTED.", DATE_FORMAT.parse("2013-10-10"))
             );
 
-            result.add(new MsiItemMock(DATE_FORMAT.parse("2013-10-07"), "Inoperative Beacon", 63.6978333, -051.6053333, "Greenland West", "Kangerluarsoruseq",
-                    "NAVIGATIONAL WARNING JOINT ARCTIC COMMAND NO. 016/13. " +
+            result.add(new MsiItemMock(DATE_FORMAT.parse("2013-10-07"), "Inoperative Beacon (TEST)", 63.6978333, -051.6053333, "Greenland West", "Kangerluarsoruseq",
+                    "TEST - NAVIGATIONAL WARNING JOINT ARCTIC COMMAND NO. 016/13. " +
                             "WEST COAST OF GREENLAND, KANGERLUARSORUSEQ. " +
                             "RACON-BEACON ON SAATTUT IN POSITION " +
                             "63-41.87N 051-36.32W IS REPORTED INOPERATIVE. " +
                             "ADMIRALITY LIST OF LIGHTS NO. L 5500", DATE_FORMAT.parse("2013-10-07"))
             );
 
-            result.add(new MsiItemMock(DATE_FORMAT.parse("2013-10-02"), "Shoal", 72.0283333, -056.0416667, "Greenland West", "Svartenhuk",
-                    "NAVIGATIONAL WARNING JOINT ARCTIC COMMAND NO. 017/13. " +
+            result.add(new MsiItemMock(DATE_FORMAT.parse("2013-10-02"), "Shoal (TEST)", 72.0283333, -056.0416667, "Greenland West", "Svartenhuk",
+                    "TEST - NAVIGATIONAL WARNING JOINT ARCTIC COMMAND NO. 017/13. " +
                             "WEST COAST OF GREENLAND, SVARTENHUK PENINSULA " +
                             "AN 8 METER SHOAL HAS BEEN REPORTED IN POS 72 01.7N 056 02.5W " +
                             "GREENLANDIC CHART NO 1600", DATE_FORMAT.parse("2013-10-02"))
             );
 
-            result.add(new MsiItemMock(DATE_FORMAT.parse("2013-09-28"), "Lost Trawl", 68.1900000, -053.9583333, "Greenland West", "Simiutarsuup",
-                    "NAVIGATIONAL WARNING JOINT ARCTIC COMMAND NO. 018/13. " +
+            result.add(new MsiItemMock(DATE_FORMAT.parse("2013-09-28"), "Lost Trawl (TEST)", 68.1900000, -053.9583333, "Greenland West", "Simiutarsuup",
+                    "TEST - NAVIGATIONAL WARNING JOINT ARCTIC COMMAND NO. 018/13. " +
                             "WEST COAST OF GREENLAND, SIMIUTARSUUP " +
                             "A TRAWL(OLD) HAS BEEN REPORTED FLOATING IN POS 68 11,4N " +
                             "053 57,5w GREENLANDIC CHART NO 1416", DATE_FORMAT.parse("2013-09-28"))
             );
 
-            result.add(new MsiItemMock(DATE_FORMAT.parse("2013-09-30"), "Lost Dinghy", 64.142664, -51.615843, "Greenland West", "Nuuk Fiord",
-                    "NAVIGATIONAL WARNING JOINT ARCTIC COMMAND NO. 019/13. " +
+            result.add(new MsiItemMock(DATE_FORMAT.parse("2013-09-30"), "Lost Dinghy (TEST)", 64.142664, -51.615843, "Greenland West", "Nuuk Fiord",
+                    "TEST - NAVIGATIONAL WARNING JOINT ARCTIC COMMAND NO. 019/13. " +
                             "WEST COAST OF GREENLAND, NUUK FIORD. " +
                             "FISHINGVESSEL PITA HAVE LOST A DINGHY YESTERDAY 30 SEP " +
                             "2230 LMT NE OF NUUK AIRPORT.THE DINGHY IS A RYDS 535 WITH " +
