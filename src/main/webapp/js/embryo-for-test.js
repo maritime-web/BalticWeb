7cef9cddc7adf95cd17f3d9d69762d6c97807655
Christian Hvid <chvid@Christians-MacBook-Pro.local>
2013-10-23 16:08:29 +0200

    Bug unfixed

--- src/main/webapp/js/embryo-for-test.js
+++ src/main/webapp/js/embryo-for-test.js
@@ -30,4 +30,4 @@ embryo.eventbus.EmbryoReadyEvent = function() {
 };
 
 embryo.eventbus.registerShorthand(embryo.eventbus.EmbryoReadyEvent, "ready");
-embryo.eventbus.registerShorthand(embryo.eventbus.AuthenticatedEvent, "authenticated");
+embryo.eventbus.registerShorthand(embryo.eventbus.EmbryoReadyEvent, "authenticated");
