34c8793b2b18d14958ca0b54cc2907289a82de28
Christian Hvid <chvid@Christians-MacBook-Pro.local>
2013-10-23 14:21:13 +0200

    Formatting

--- src/main/webapp/js/utils.js
+++ src/main/webapp/js/utils.js
@@ -1,6 +1,3 @@
-/**
- * Method for setting a cookie.
- */
 function setCookie(c_name, value, exdays) {
     var exdate = new Date();
     exdate.setDate(exdate.getDate() + exdays);
@@ -9,18 +6,15 @@ function setCookie(c_name, value, exdays) {
     document.cookie = c_name + "=" + c_value;
 }
 
-/**
- * Method for getting a cookie.
- */
 function getCookie(c_name) {
     var i, x, y, ARRcookies = document.cookie.split(";");
     for (i = 0; i < ARRcookies.length; i++) {
-	x = ARRcookies[i].substr(0, ARRcookies[i].indexOf("="));
-	y = ARRcookies[i].substr(ARRcookies[i].indexOf("=") + 1);
-	x = x.replace(/^\s+|\s+$/g, "");
-	if (x == c_name) {
-	    return unescape(y);
-	}
+        x = ARRcookies[i].substr(0, ARRcookies[i].indexOf("="));
+        y = ARRcookies[i].substr(ARRcookies[i].indexOf("=") + 1);
+        x = x.replace(/^\s+|\s+$/g, "");
+        if (x == c_name) {
+            return unescape(y);
+        }
     }
 }
 
