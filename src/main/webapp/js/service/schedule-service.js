1865a06247e5c0af56dd6f8f372a8cd4a672d3c1
tejl <jesper@lundogbendsen.dk>
2013-11-04 16:45:37 +0100

    EMBRYO-28: rreplaced ScheduleService.getYourActive by getActiveVoyage

--- src/main/webapp/js/service/schedule-service.js
+++ src/main/webapp/js/service/schedule-service.js
@@ -18,18 +18,41 @@
         var currentSchedule = 'schedule_current';
         var activeVoyage = 'voyage_active';
 
-        return {
-            getYourActive : function(mmsi, callback) {
-                var voyageStr = sessionStorage.getItem(activeVoyage + mmsi);
-                if (!voyageStr) {
-                    var remoteCall = function(onSuccess) {
-                        $http.get(scheduleUrl + 'active/' + mmsi).success(onSuccess);
-                    };
+        function buildVoyageInfo(index, schecule) {
+            var result = {
+                id : schecule.voyages[index].maritimeId,
+                dep : schecule.voyages[index].berthName,
+                depEta : schecule.voyages[index].departure,
+                crew : schecule.voyages[index].crew,
+                passengers : schecule.voyages[index].passengers,
+            };
+            if (index < schecule.voyages.length - 1) {
+                result.des = schecule.voyages[index + 1].berthName;
+                result.desEta = schecule.voyages[index + 1].arrival;
+            }
+            return result;
+        }
 
-                    SessionStorageService.getItem(activeVoyage + mmsi, callback, remoteCall);
-                } else {
-                    callback(JSON.parse(voyageStr));
+        return {
+            getActiveVoyage : function(mmsi, routeId, callback) {
+                function findVoyageIndex(schedule) {
+                    for ( var index = 0; index < schedule.voyages.length; index++) {
+                        if (schedule.voyages[index].route.id === routeId) {
+                            return index;
+                        }
+                    }
+                    return null;
                 }
+
+                this.getSchedule(mmsi, function(schedule) {
+                    var index = findVoyageIndex(schedule);
+                    if (index == null) {
+                        callback(null);
+                        return;
+                    }
+                    var voyageInfo = buildVoyageInfo(index, schedule);
+                    callback(voyageInfo);
+                });
             },
             getSchedule : function(mmsi, callback) {
                 var remoteCall = function(onSuccess) {
@@ -45,18 +68,6 @@
                         }
                     }
                     return null;
-                }
-                function buildVoyageInfo(index, schecule, callback) {
-                    var result = {
-                        id : schecule.voyages[index].maritimeId,
-                        dep : schecule.voyages[index].berthName,
-                        depEta : schecule.voyages[index].departure,
-                    };
-                    if (index < schecule.voyages.length - 1) {
-                        result.des = schecule.voyages[index + 1].berthName;
-                        result.desEta = schecule.voyages[index + 1].arrival;
-                    }
-                    return result;
                 }
                 this.getSchedule(mmsi, function(schedule) {
                     var index = findVoyageIndex(voyageId, schedule);
