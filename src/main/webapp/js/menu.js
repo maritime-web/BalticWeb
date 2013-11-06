93617575abd19f557719b3974ae6ed66742cff24
Christian Hvid <chvid@Christians-MacBook-Pro.local>
2013-11-06 11:18:12 +0100

    Permission controls in menu

--- src/main/webapp/js/menu.js
+++ src/main/webapp/js/menu.js
@@ -30,12 +30,30 @@ embryo.eventbus.registerShorthand(embryo.eventbus.GroupChangedEvent, "groupChang
                 }
             });
 
-            if (!embryoAuthenticated) {
-                embryo.authenticated(function() {
-                    embryo.eventbus.fireEvent(embryo.eventbus.GroupChangedEvent(url.substring(url.lastIndexOf("/")+1)));
-                })
-            } else {
+            $(".navtabs a", $element).each(function(k, v) {
+                var href = $(v).attr("href");
+                if (url.indexOf(href, url.length - href.length) >= 0) {
+                    $(v).parent("li").addClass("active");
+                } else {
+                    $(v).parent("li").removeClass("active");
+                }
+            });
+
+            $("li", $element).each(function(k, v) {
+                if ($(v).attr("requires-permission")) $(v).css("display", "none");
+            })
+
+            function authenticated() {
                 embryo.eventbus.fireEvent(embryo.eventbus.GroupChangedEvent(url.substring(url.lastIndexOf("/")+1)));
+                $("li", $element).each(function(k, v) {
+                    if (embryo.authentication.permissions.indexOf($(v).attr("requires-permission")) >= 0) $(v).css("display", "block");
+                })
+            }
+
+            if (!embryoAuthenticated) {
+                embryo.authenticated(authenticated);
+            } else {
+                authenticated();
             }
 
         });
