8db5d951fa041c92714eee57545e3d0a67674ca1
tejl <jesper@lundogbendsen.dk>
2013-11-07 11:46:20 +0100

    EMBRYO-49, EMBRYO-38: cleanups

--- src/main/webapp/js/service/route-service.js
+++ src/main/webapp/js/service/route-service.js
@@ -14,9 +14,6 @@
     var module = angular.module('embryo.routeService', [ 'embryo.storageServices' ]).service('RouteService',
 
     function($rootScope, $http, SessionStorageService, LocalStorageService) {
-
-        var active = 'route_active';
-
         var routeKey = function(routeId) {
             return 'route_' + routeId;
         };
@@ -30,58 +27,17 @@
 
                 remoteCall(callback);
             },
-            getYourActive : function(mmsi, callback) {
-                var remoteCall = function(onSuccess) {
-                    var url = embryo.baseUrl + 'rest/route/active/' + mmsi;
-                    $http.get(url).success(onSuccess);
-                };
-
-                // active route is maintained in SessionStorage as two
-                // key value
-                // pairs:
-                // 'route_active' -> someRouteId
-                // 'route_someRouteId' -> Route (stringified JS object)
-                SessionStorageService.getItem(active, function(routeId) {
-                    if (routeId) {
-                        SessionStorageService.getItem(routeKey(routeId), callback, remoteCall);
-                    } else {
-                        remoteCall(function(activeRoute) {
-                            if (activeRoute && Object.keys(activeRoute).length > 0) {
-                                SessionStorageService.setItem(active, activeRoute.id);
-                                SessionStorageService.setItem(routeKey(activeRoute.id), activeRoute);
-                                callback(activeRoute);
-                            } else {
-                                SessionStorageService.removeItem(active);
-                                callback(null);
-                            }
-                        });
-                    }
-                });
-            },
             setActiveRoute : function(routeId, activity, callback) {
-                var activeRoute = {
+                $http.put(embryo.baseUrl + 'rest/route/activate/', {
                     routeId : routeId,
                     active : activity
-                };
-
-                $http.put(embryo.baseUrl + 'rest/route/activate/', activeRoute).success(function() {
-                    if (activity) {
-                        SessionStorageService.setItem(active, routeId);
-                    } else {
-                        SessionStorageService.removeItem(active);
-                    }
-                    callback();
-                });
-            },
-            clearActiveFromCache : function() {
-                SessionStorageService.removeItem(active);
+                }).success(callback);
             },
             getRoute : function(routeId, callback) {
                 // should routes be cached?
                 var remoteCall = function(onSuccess) {
                     $http.get(embryo.baseUrl + 'rest/route/' + routeId).success(onSuccess);
                 };
-
                 SessionStorageService.getItem(routeKey(routeId), callback, remoteCall);
             },
             save : function(route, voyageId, callback) {
