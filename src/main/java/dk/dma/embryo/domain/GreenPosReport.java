95e40f91b9252c8292a77d707d9a207fe14eb296
tejl <jesper@lundogbendsen.dk>
2013-11-12 12:52:37 +0100

    EMBRYO-66: vesselname, mmsi and call sign required

--- src/main/java/dk/dma/embryo/domain/GreenPosReport.java
+++ src/main/java/dk/dma/embryo/domain/GreenPosReport.java
@@ -55,8 +55,10 @@ public abstract class GreenPosReport extends BaseEntity<Long> {
     @NotNull
     private String vesselName;
 
+    @NotNull
     private Long vesselMmsi;
 
+    @NotNull
     private String vesselCallSign;
 
     @Valid
