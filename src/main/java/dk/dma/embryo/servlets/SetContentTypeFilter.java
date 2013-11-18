2519b3f51cb8201956918f3ebfee18756be2a538
Christian Hvid <chvid@Christians-MacBook-Pro.local>
2013-11-18 09:29:15 +0100

    Cache functionality

--- src/main/java/dk/dma/embryo/servlets/SetContentTypeFilter.java
+++ src/main/java/dk/dma/embryo/servlets/SetContentTypeFilter.java
@@ -15,7 +15,12 @@
  */
 package dk.dma.embryo.servlets;
 
-import javax.servlet.*;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
 import java.io.IOException;
 
 public class SetContentTypeFilter implements Filter {
