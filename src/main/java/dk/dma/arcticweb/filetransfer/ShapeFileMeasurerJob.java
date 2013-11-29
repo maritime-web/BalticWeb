3947b31d0d5a4d0863762d3e30ad98f9e29e12de
Christian Hvid <chvid@Christians-MacBook-Pro.local>
2013-11-29 12:31:26 +0100

    Must comply to standards

--- src/main/java/dk/dma/arcticweb/filetransfer/ShapeFileMeasurerJob.java
+++ src/main/java/dk/dma/arcticweb/filetransfer/ShapeFileMeasurerJob.java
@@ -31,7 +31,12 @@ import javax.inject.Inject;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 import java.util.zip.GZIPOutputStream;
 
 @Singleton
