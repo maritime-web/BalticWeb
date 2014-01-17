43b78efceac61d540068bd3a229f46bf2ba0d939
tejl <jesper@lundogbendsen.dk>
2014-01-17 10:48:45 +0100

    EMBRYO-215: distance rings based on Max SOG. Labels for Nearest Vessel expanded with Max SOG and vessel name.

--- src/main/webapp/js/layer/distance-layer.js
+++ src/main/webapp/js/layer/distance-layer.js
@@ -1,28 +1,28 @@
 function DistanceLayer() {
-    this.init = function () {
+    this.init = function() {
         this.layers.lines = new OpenLayers.Layer.Vector("Vessel - Distance Layer - Line", {
-            styleMap: new OpenLayers.StyleMap({
-                "default": new OpenLayers.Style({
-                    fillColor: "#f80",
-                    fillOpacity: 0.1,
-                    strokeWidth: 2,
-                    strokeColor: "#f80",
-                    strokeOpacity: 0.7
+            styleMap : new OpenLayers.StyleMap({
+                "default" : new OpenLayers.Style({
+                    fillColor : "#f80",
+                    fillOpacity : 0.1,
+                    strokeWidth : 2,
+                    strokeColor : "#f80",
+                    strokeOpacity : 0.7
                 })
             })
         });
 
         this.layers.labels = new OpenLayers.Layer.Vector("Vessel - Distance Layer - Labels", {
-            styleMap: new OpenLayers.StyleMap({
-                "default": new OpenLayers.Style({
+            styleMap : new OpenLayers.StyleMap({
+                "default" : new OpenLayers.Style({
                     label : "${label}",
                     fontColor : "black",
                     fontSize : "11px",
                     fontFamily : embryo.defaultFontFamily,
                     fontWeight : "normal",
                     labelAlign : "cm",
-                    labelXOffset : 0,
-                    labelYOffset : -15,
+                    labelXOffset : "${labelXOffset}",
+                    labelYOffset : "${labelYOffset}",
                     labelOutlineColor : "#fff",
                     labelOutlineWidth : 2,
                     labelOutline : 1
@@ -30,21 +30,32 @@ function DistanceLayer() {
             })
         });
     };
-    
-    this.drawDistanceCircles = function (vessel, vesselDetails) {
+
+    this.drawDistanceCircles = function(vessel, vesselDetails) {
         this.layers.lines.removeAllFeatures();
         this.layers.labels.removeAllFeatures();
-        this.layers.lines.addFeatures(embryo.adt.createRing(vessel.x, vessel.y, vesselDetails.ais.sog * 3 * 1.852, 3));
+        this.layers.lines.addFeatures(embryo.adt.createRing(vessel.x, vessel.y, vessel.msog * 3 * 1.852, 3));
+
+        if (vessel.msog) {
+            var labelFeature = new OpenLayers.Feature.Vector(embryo.map.createPoint(vessel.x, vessel.y));
+            // (parseFloat(vessel.x) + parseFloat(v.vessel.x)) / 2,
+            // (parseFloat(vessel.y) + parseFloat(v.vessel.y)) / 2
+            labelFeature.attributes = {
+                label : "Based on maximum recorded SOG: " + vessel.msog + " NM",
+                labelXOffset : 140,
+                labelYOffset : -15
+            }
+            this.layers.labels.addFeatures([ labelFeature ]);
+        }
     };
 
-
-    this.drawNearestVessels = function (vessel, vesselDetails, allVessels) {
+    this.drawNearestVessels = function(vessel, vesselDetails, allVessels) {
         this.layers.lines.removeAllFeatures();
         this.layers.labels.removeAllFeatures();
 
         var vessels = [];
 
-        $.each(allVessels, function (k,v) {
+        $.each(allVessels, function(k, v) {
             if (v.mmsi != vessel.mmsi) {
                 distance = embryo.adt.measureDistanceGc(vessel.x, vessel.y, v.x, v.y);
                 var o = {
@@ -59,45 +70,66 @@ function DistanceLayer() {
         });
 
         vessels.sort(function(a, b) {
-            if(a.timeInMinutes == Infinity && b.timeInMinutes == Infinity){
+            if (a.timeInMinutes == Infinity && b.timeInMinutes == Infinity) {
                 return 0;
             }
-            if(a.timeInMinutes == Infinity && b.timeInMinutes != Infinity){
+            if (a.timeInMinutes == Infinity && b.timeInMinutes != Infinity) {
                 return 100;
             }
-            if(a.timeInMinutes != Infinity && b.timeInMinutes == Infinity){
+            if (a.timeInMinutes != Infinity && b.timeInMinutes == Infinity) {
                 return -100;
             }
-            
+
             return a.timeInMinutes - b.timeInMinutes;
         });
 
-        for (var i = 0; i < 5; i++) {
+//        function getVesselsWithYOffsets() {
+//            var yCoords = [];
+//            for ( var i = 0; i < 5; i++) {
+//                
+//                embryo.map.internalMap.getViewPortPxFromLonLat();
+//                
+//                yCoords.push({
+//                    y : vessels[i].vessel.y,
+//                    vessel : vessels[i].vessel
+//                });
+//            }
+//            yCoords.sort(function(coord1, coord2){
+//                return coord1.y - coord2.y;
+//            });
+//            
+//            console.log(yCoords);
+//
+//            for ( var i = 1; i < 5; i++) {
+//                var diff = yCoords[i-1].y - yCoords[i].y;
+//                console.log(diff);
+//                if(diff <= 16){
+////                    for ( var j = i; j < 5; j++) {
+////                        yCoords[j].y += 15;
+////                    }
+//                }
+//            }
+//        }
+//        
+//        var vesselsWithYOffsets = getVesselsWithYOffsets();
+
+        for ( var i = 0; i < 5; i++) {
             var v = vessels[i];
 
-            this.layers.lines.addFeatures([new OpenLayers.Feature.Vector(new OpenLayers.Geometry.LineString([
-                embryo.map.createPoint(vessel.x, vessel.y), embryo.map.createPoint(v.vessel.x, v.vessel.y)
-            ]))]);
+            this.layers.lines.addFeatures([ new OpenLayers.Feature.Vector(new OpenLayers.Geometry.LineString([
+                    embryo.map.createPoint(vessel.x, vessel.y), embryo.map.createPoint(v.vessel.x, v.vessel.y) ])) ]);
 
-            var labelFeature = new OpenLayers.Feature.Vector(embryo.map.createPoint(
-                v.vessel.x, v.vessel.y
-                // (parseFloat(vessel.x) + parseFloat(v.vessel.x)) / 2, (parseFloat(vessel.y) + parseFloat(v.vessel.y)) / 2
-            ));
-
-            if (v.msog != Infinity) {
-                labelFeature.attributes = {
-                    label: formatNauticalMile(v.distance) + " " + formatHour(v.distance / (v.vessel.msog * 1.852))+ " hours"
-                }
-            } else {
-                labelFeature.attributes = {
-                    label: formatNauticalMile(v.distance)
-                }
-            }
-
-
-            this.layers.labels.addFeatures([
-                labelFeature
-            ]);
+            var labelFeature = new OpenLayers.Feature.Vector(embryo.map.createPoint(v.vessel.x, v.vessel.y));
+            labelFeature.attributes = {
+                labelXOffset : 0,
+                labelYOffset : -15,
+                label : v.vessel.name
+                        + ": "
+                        + formatNauticalMile(v.distance)
+                        + (v.vessel.msog == Infinity ? "" : ", " + formatHour(v.distance / (v.vessel.msog * 1.852))
+                                + " hours, Max SOG " + v.vessel.msog)
+            };
+            this.layers.labels.addFeatures([ labelFeature ]);
         }
     };
 }
@@ -105,12 +137,12 @@ function DistanceLayer() {
 DistanceLayer.prototype = new EmbryoLayer();
 
 /*
- * Can be used to create only one distance layer instance and reuse this as  
+ * Can be used to create only one distance layer instance and reuse this as
  */
 var DistanceLayerSingleton = {
     instance : null,
-    getInstance : function(){
-        if(this.instance == null){
+    getInstance : function() {
+        if (this.instance == null) {
             this.instance = new DistanceLayer();
             addLayerToMap("vessel", this.instance, embryo.map);
         }
