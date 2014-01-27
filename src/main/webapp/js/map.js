38f0ac031ec61531b46c48be299e8d305d1b7569
tejl <jesper@lundogbendsen.dk>
2014-01-27 21:26:44 +0100

    EMBRYO-223: blue area in top of map no longer shown on big screens when using the Zoom All Arctic button.

--- src/main/webapp/js/map.js
+++ src/main/webapp/js/map.js
@@ -20,27 +20,23 @@ embryo.eventbus.registerShorthand(embryo.eventbus.UnHighLightEvent, "unhighlight
 embryo.eventbus.registerShorthand(embryo.eventbus.MapInitialized, "mapInitialized");
 
 $(function() {
-    var selectLayerByGroup = { };
+    var selectLayerByGroup = {};
 
-    var controlsByGroup = { };
+    var controlsByGroup = {};
 
     var e1 = new OpenLayers.Bounds(-179, -89, 179, 89);
 
-    e1.transform(
-        new OpenLayers.Projection("EPSG:4326"),
-        new OpenLayers.Projection(embryo.projection)
-    );
+    e1.transform(new OpenLayers.Projection("EPSG:4326"), new OpenLayers.Projection(embryo.projection));
 
     var map = new OpenLayers.Map({
         div : "map",
-        controls: [
-            new OpenLayers.Control.Navigation(
-                { dragPanOptions: { enableKinetic: false } }
-            ),
-            new OpenLayers.Control.Zoom()
-        ],
-        projection: embryo.projection,
-        maxExtent: e1,
+        controls : [ new OpenLayers.Control.Navigation({
+            dragPanOptions : {
+                enableKinetic : false
+            }
+        }), new OpenLayers.Control.Zoom() ],
+        projection : embryo.projection,
+        maxExtent : e1,
         fractionalZoom : false
     });
 
@@ -69,7 +65,7 @@ $(function() {
     });
 
     embryo.map = {
-        add: function(d) {
+        add : function(d) {
             if (d.group && selectLayerByGroup[d.group] == null) {
                 selectLayerByGroup[d.group] = [];
                 controlsByGroup[d.group] = [];
@@ -77,31 +73,34 @@ $(function() {
 
             if (d.layer) {
                 map.addLayer(d.layer);
-                if (d.select && d.group) selectLayerByGroup[d.group].push(d.layer);
+                if (d.select && d.group)
+                    selectLayerByGroup[d.group].push(d.layer);
             }
 
             if (d.control) {
                 map.addControl(d.control);
-                if (d.group) controlsByGroup[d.group].push(d.control);
+                if (d.group)
+                    controlsByGroup[d.group].push(d.control);
             }
         },
-        createPoint: function(longitude, latitude) {
-            return new OpenLayers.Geometry.Point(longitude, latitude)
-                .transform(new OpenLayers.Projection("EPSG:4326"), map.getProjectionObject());
+        createPoint : function(longitude, latitude) {
+            return new OpenLayers.Geometry.Point(longitude, latitude).transform(new OpenLayers.Projection("EPSG:4326"),
+                    map.getProjectionObject());
         },
-        select: function(feature) {
+        select : function(feature) {
             selectControl.unselectAll();
-            if (feature != null) selectControl.select(feature);
+            if (feature != null)
+                selectControl.select(feature);
         },
-        zoomToExtent: function(layers) {
+        zoomToExtent : function(layers) {
             var extent = new OpenLayers.Bounds();
-            
-            extent.bottom= 9999999;
-            extent.left= 9999999;
-            extent.right= -9999999;
-            extent.top= -9999999;
-            
-            for (var i in layers) {
+
+            extent.bottom = 9999999;
+            extent.left = 9999999;
+            extent.right = -9999999;
+            extent.top = -9999999;
+
+            for ( var i in layers) {
                 var e = layers[i].getDataExtent();
                 if (e != null) {
                     extent.bottom = Math.min(extent.bottom, e.bottom);
@@ -111,7 +110,8 @@ $(function() {
                 }
             }
 
-            // nudge extent so that the left overlay does not overlay the zoomed layers
+            // nudge extent so that the left overlay does not overlay the zoomed
+            // layers
 
             var deltaV = extent.top - extent.bottom;
             var deltaH = extent.right - extent.left;
@@ -123,40 +123,45 @@ $(function() {
 
             map.zoomToExtent(extent);
         },
-        setCenter: function(longitude, latitude, zoom) {
-            map.setCenter(transformPosition(longitude, latitude), zoom);
-        }, 
-        internalMap: map,
-        createClickControl: function(handler) {
+        setCenter : function(longitude, latitude, zoom) {
+            var pos = transformPosition(longitude, latitude);
+
+            // set position to find center in pixels
+            map.setCenter(pos, zoom);
+            
+            var newCenterPx = map.getPixelFromLonLat(pos);
+            // check if new center will provoke a blue rectangel in the top when using OSM tile Map (no tiles on the pole)
+            // and correct center value to remove blue rectangel
+            if (newCenterPx.y > (1000 - 50)/2) {
+                newCenterPx.y += (newCenterPx.y - (1000 - 50)/2);
+                pos = map.getLonLatFromPixel(newCenterPx);
+            }
+
+            map.setCenter(pos, zoom);
+        },
+        internalMap : map,
+        createClickControl : function(handler) {
             var Control = OpenLayers.Class(OpenLayers.Control, {
-                defaultHandlerOptions: {
-                    'single': true,
-                    'double': false,
-                    'pixelTolerance': 0,
-                    'stopSingle': false,
-                    'stopDouble': false
+                defaultHandlerOptions : {
+                    'single' : true,
+                    'double' : false,
+                    'pixelTolerance' : 0,
+                    'stopSingle' : false,
+                    'stopDouble' : false
                 },
 
-                initialize: function(options) {
-                    this.handlerOptions = OpenLayers.Util.extend(
-                        {}, this.defaultHandlerOptions
-                    );
-                    OpenLayers.Control.prototype.initialize.apply(
-                        this, arguments
-                    );
-                    this.handler = new OpenLayers.Handler.Click(
-                        this, {
-                            'click': this.trigger
-                        }, this.handlerOptions
-                    );
+                initialize : function(options) {
+                    this.handlerOptions = OpenLayers.Util.extend({}, this.defaultHandlerOptions);
+                    OpenLayers.Control.prototype.initialize.apply(this, arguments);
+                    this.handler = new OpenLayers.Handler.Click(this, {
+                        'click' : this.trigger
+                    }, this.handlerOptions);
                 },
 
-                trigger: function(e) {
+                trigger : function(e) {
                     var lonlat1 = this.map.getLonLatFromPixel(e.xy);
-                    var lonlat = new OpenLayers.LonLat(lonlat1.lon, lonlat1.lat).transform(
-                        this.map.getProjectionObject(),
-                        new OpenLayers.Projection("EPSG:4326")
-                    );
+                    var lonlat = new OpenLayers.LonLat(lonlat1.lon, lonlat1.lat).transform(this.map
+                            .getProjectionObject(), new OpenLayers.Projection("EPSG:4326"));
                     return handler(lonlat);
                 }
             });
@@ -170,29 +175,30 @@ $(function() {
     // time. Also see below link.
     // http://gis.stackexchange.com/questions/13886/how-to-select-multiple-features-from-multiple-layers-in-openlayers
     // and http://openlayers.org/dev/examples/select-feature-multilayer.html
-    
+
     // Reacting on select / unselect is performed like:
     // someLayer.events.on({
     // "featureselected" : function(e){ ... },
-            // "featureunselected" : function(e){ ... }
+    // "featureunselected" : function(e){ ... }
     // });
-    
+
     // add select control, registered for no layers
     // Layers will be registered on control through usage of
     // addSelectableLayer
-    
+
     map.events.includeXY = true;
-    
-    /*map.addControl(hoverControl);
-    hoverControl.activate();*/
-    
+
+    /*
+     * map.addControl(hoverControl); hoverControl.activate();
+     */
+
     map.addControl(selectControl);
     selectControl.activate();
-    
+
     /**
      * Transforms a position to a position that can be used by OpenLayers. The
      * transformation uses OpenLayers.Projection("EPSG:4326").
-     *
+     * 
      * @param lon
      *            The longitude of the position to transform
      * @param lat
@@ -200,10 +206,8 @@ $(function() {
      * @returns The transformed position as a OpenLayers.LonLat instance.
      */
     function transformPosition(lon, lat) {
-        return new OpenLayers.LonLat(lon, lat).transform(
-            new OpenLayers.Projection("EPSG:4326"),
-            map.getProjectionObject()
-        );
+        return new OpenLayers.LonLat(lon, lat).transform(new OpenLayers.Projection("EPSG:4326"), map
+                .getProjectionObject());
     }
 
     /**
@@ -211,16 +215,17 @@ $(function() {
      */
     function saveViewCookie() {
         var center = map.getCenter();
-        setCookie("dma-ais-zoom-"+ embryo.authentication.userName, map.zoom, 30);
-        var lonlat = new OpenLayers.LonLat(map.center.lon, map.center.lat).
-            transform(
-                map.getProjectionObject(), // from Spherical Mercator Projection
-                new OpenLayers.Projection("EPSG:4326") // to WGS 1984
-            );
+        setCookie("dma-ais-zoom-" + embryo.authentication.userName, map.zoom, 30);
+        var lonlat = new OpenLayers.LonLat(map.center.lon, map.center.lat).transform(map.getProjectionObject(), // from
+        // Spherical
+        // Mercator
+        // Projection
+        new OpenLayers.Projection("EPSG:4326") // to WGS 1984
+        );
         setCookie("dma-ais-lat-" + embryo.authentication.userName, lonlat.lat, 30);
         setCookie("dma-ais-lon-" + embryo.authentication.userName, lonlat.lon, 30);
     }
-    
+
     /**
      * Get settings from cookies
      */
@@ -234,17 +239,17 @@ $(function() {
             embryo.map.setCenter(-65, 70, 3);
         }
     }
-    
+
     map.events.register("movestart", map, function() {
         embryo.eventbus.fireEvent(embryo.eventbus.UnHighLightEvent(null));
     });
 
     map.events.register("moveend", map, saveViewCookie);
-    
+
     // Position zoom control to the right
-    
+
     function moveZoomControl() {
-        $(".olControlZoom").css("left", $("#map").width()-40);
+        $(".olControlZoom").css("left", $("#map").width() - 40);
         $(".olControlZoom").css("top", 50);
     }
 
@@ -252,13 +257,10 @@ $(function() {
     moveZoomControl();
 
     function setupOsmMap() {
-        var osm = new OpenLayers.Layer.OSM(
-            "OSM",
-            embryo.authentication.osm, {
-                'layers' : 'basic',
-                'isBaseLayer' : true
-            }
-        );
+        var osm = new OpenLayers.Layer.OSM("OSM", embryo.authentication.osm, {
+            'layers' : 'basic',
+            'isBaseLayer' : true
+        });
         map.addLayer(osm);
         map.setBaseLayer(osm);
     }
@@ -266,16 +268,13 @@ $(function() {
     function setupVectorMap(name) {
         var e1 = new OpenLayers.Bounds(-179, -89, 179, 89);
 
-        e1.transform(
-            new OpenLayers.Projection("EPSG:4326"),
-            embryo.projection
-        );
+        e1.transform(new OpenLayers.Projection("EPSG:4326"), embryo.projection);
 
         var blankLayer = new OpenLayers.Layer("Blank", {
-            isBaseLayer: true,
-            numZoomLevels: 19,
-            projection: map.projection,
-            maxExtent: e1
+            isBaseLayer : true,
+            numZoomLevels : 19,
+            projection : map.projection,
+            maxExtent : e1
         });
 
         map.addLayer(blankLayer);
@@ -285,9 +284,9 @@ $(function() {
 
         addLayerToMap("world", layer, embryo.map);
         embryo.ice.service.shapes({
-            ids: "static." + name,
-            exponent: 2,
-            delta:true
+            ids : "static." + name,
+            exponent : 2,
+            delta : true
         }, function(error, data) {
             if (data) {
                 layer.draw(data);
@@ -298,7 +297,7 @@ $(function() {
     }
 
     function removeMapLayers() {
-        $.each(["Blank", "World", "OSM"], function(k, v) {
+        $.each([ "Blank", "World", "OSM" ], function(k, v) {
             $.each(map.getLayersByName(v), function(k, v) {
                 map.removeLayer(v);
             })
@@ -309,18 +308,19 @@ $(function() {
         removeMapLayers();
 
         switch (embryo.baseMap) {
-            case "osm":
-                setupOsmMap();
-                break;
-            default:
-                setupVectorMap(embryo.baseMap)
-                break;
+        case "osm":
+            setupOsmMap();
+            break;
+        default:
+            setupVectorMap(embryo.baseMap)
+            break;
         }
     }
 
     embryo.authenticated(function() {
-        var cookieMapName = getCookie("dma-ais-map-"+ embryo.authentication.userName);
-        if (cookieMapName) embryo.baseMap = cookieMapName;
+        var cookieMapName = getCookie("dma-ais-map-" + embryo.authentication.userName);
+        if (cookieMapName)
+            embryo.baseMap = cookieMapName;
 
         setupBaseMap();
 
@@ -336,11 +336,13 @@ $(function() {
     embryo.groupChanged(function(e) {
         selectControl.unselectAll();
         selectControl.setLayer(selectLayerByGroup[e.groupId]);
-        for (var i in controlsByGroup) {
+        for ( var i in controlsByGroup) {
             if (i == e.groupId) {
-                for (var j in controlsByGroup[i]) controlsByGroup[i][j].activate();
+                for ( var j in controlsByGroup[i])
+                    controlsByGroup[i][j].activate();
             } else {
-                for (var j in controlsByGroup[i]) controlsByGroup[i][j].deactivate();
+                for ( var j in controlsByGroup[i])
+                    controlsByGroup[i][j].deactivate();
             }
         }
     });
@@ -358,8 +360,10 @@ $(function() {
         $("#switchBaseMapDialog .btn-primary").click(function(e) {
             var newMap;
 
-            if ($("#bsOpenStreetMap").prop("checked")) newMap = "osm";
-            if ($("#bsSimpleVectorMap").prop("checked")) newMap = "world_merc";
+            if ($("#bsOpenStreetMap").prop("checked"))
+                newMap = "osm";
+            if ($("#bsSimpleVectorMap").prop("checked"))
+                newMap = "world_merc";
 
             $("#switchBaseMapDialog").modal("hide");
 
@@ -372,4 +376,3 @@ $(function() {
 
     })
 });
-
