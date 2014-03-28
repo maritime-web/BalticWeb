a24f76791ab024177f55898cc524f6ad268a230e
tejl <jesper@lundogbendsen.dk>
2014-03-28 16:43:14 +0100

    EMBRYO-47: moved database files into common.

---
+++ embryo-common/src/main/java/dk/dma/embryo/common/db/MySql5BitBooleanDialect.java
@@ -0,0 +1,32 @@
+/* Copyright (c) 2011 Danish Maritime Authority
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package dk.dma.embryo.common.db;
+
+import org.hibernate.dialect.MySQL5Dialect;
+
+/**
+ * Solution to Hibernate defects: 
+
+ * https://hibernate.atlassian.net/browse/HHH-6935
+ * 
+ * @author Jesper Tejlgaard
+ */
+public class MySql5BitBooleanDialect extends MySQL5Dialect {
+    public MySql5BitBooleanDialect() {
+        super();
+        registerColumnType(java.sql.Types.BOOLEAN, "bit");
+    }
+}
