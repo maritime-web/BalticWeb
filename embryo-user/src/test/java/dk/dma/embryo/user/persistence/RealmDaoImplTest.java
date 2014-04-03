6f7d30218473c6bc47573fb5041eb4a25ff63bf1
tejl <jesper@lundogbendsen.dk>
2014-04-03 14:53:14 +0200

    EMBRYO-47: moved SubjectTest and RealmDaoImplTest into embryo-user

--- embryo-user/src/test/java/dk/dma/embryo/user/persistence/RealmDaoImplTest.java
+++ embryo-user/src/test/java/dk/dma/embryo/user/persistence/RealmDaoImplTest.java
@@ -13,7 +13,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this library.  If not, see <http://www.gnu.org/licenses/>.
  */
-package dk.dma.embryo.dao;
+package dk.dma.embryo.user.persistence;
 
 import static org.junit.Assert.assertEquals;
 
