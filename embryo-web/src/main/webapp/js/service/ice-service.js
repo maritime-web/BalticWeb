6ec4d9cff43834810e9b78d19bd4823e93386ec8
Jacob Avlund <jacob@avlund.dk>
2014-05-05 11:23:03 +0200

    Extra file that Git somehow didn't pick up earlier...

---
+++ embryo-web/src/main/webapp/js/service/ice-service.js
@@ -0,0 +1,41 @@
+(function() {
+    var module = angular.module('embryo.ice', []);
+
+    module.service('IceService', function($http) {
+        return {
+            providers : function(success, error) {
+                $http.get(embryo.baseUrl + "rest/ice/provider/list", {
+                    timeout : embryo.defaultTimeout
+                }).success(success).error(function(data, status, headers, config) {
+                    error(embryo.ErrorService.errorStatus(data, status, "requesting ice chart providers"), status);
+                });
+            },
+            getSelectedProvider : function(defaultProvider) {
+                var value = getCookie("selectedProvider");
+                if (!value) {
+                    setCookie("selectedProvider", defaultProvider, 365);
+                    value = defaultProvider;
+                }
+                return value;
+            },
+            setSelectedProvider : function(rovider) {
+                setCookie("selectedProvider");
+            },
+            listByProvider : function(provider, success, error) {
+                $http.get(embryo.baseUrl + "rest/ice/provider/" + provider + "/observations", {
+                    timeout : embryo.defaultTimeout,
+                }).success(success).error(function(data, status, headers, config) {
+                    error(embryo.ErrorService.errorStatus(data, status, "requesting list of ice observations"), status);
+                });
+            }        };
+    });
+
+    embryo.ice = {
+        delta : true,
+        exponent : 3
+    };
+
+    module.run(function(IceService) {
+        embryo.ice.service = IceService;
+    });
+})();
