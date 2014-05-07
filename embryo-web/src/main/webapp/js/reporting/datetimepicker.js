8ad1760227352800ff7b35f0236fc5793cb41a83
tejl <jesper@lundogbendsen.dk>
2014-05-07 10:37:07 +0200

    EMBRYO-255: corrected icons for datetime picker

--- embryo-web/src/main/webapp/js/reporting/datetimepicker.js
+++ embryo-web/src/main/webapp/js/reporting/datetimepicker.js
@@ -13,7 +13,7 @@
             replace : true,
             template : '<div class="input-group date" data-date-format="YYYY-MM-DD hh:mm">'
                     + '<input type="text" class="input-sm form-control" data-format="yyyy-MM-dd hh:mm" />'
-                    + '<span class="input-group-addon">' + ' <span class="glyphicon glyphicon-calendar"></span>'
+                    + '<span class="input-group-addon">' + ' <span class="fa fa-calendar"></span>'
                     + '</span>' + '</div>',
             //              
             // '<div>' +
@@ -26,7 +26,13 @@
                     pick12HourFormat : false,
                     useSeconds : false,
                     useCurrent : true,
-                    showToday : true
+                    showToday : true,
+                    icons : {
+                            time: 'fa fa-clock-o',
+                            date: 'fa fa-calendar',
+                            up: 'fa fa-chevron-up',
+                            down: 'fa fa-chevron-down'
+                        }
                 });
                 var picker = $(element).data('DateTimePicker');
                 
