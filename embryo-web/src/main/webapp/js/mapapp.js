100d3c93136058bc4b0f51ba7901e2f7b136f2a2
tejl <jesper@lundogbendsen.dk>
2014-05-14 15:36:28 +0200

    EMBRYO-310: moved application scripts to bottom of page. Initializing angular appliation as part of js file.

--- embryo-web/src/main/webapp/js/mapapp.js
+++ embryo-web/src/main/webapp/js/mapapp.js
@@ -3,5 +3,9 @@
     angular.module('embryo.map', [ 'ui.bootstrap', 'embryo.menu', 'embryo.routeService', 'embryo.greenpos',
             'embryo.vessel.controller', 'embryo.ice', 'embryo.msi', 'embryo.routeUpload', 'embryo.schedule',
             'embryo.routeEdit', 'embryo.decimal', 'embryo.authentication', 'embryo.shape', 'embryo.zoom' ]);
+    
+    angular.element(document).ready(function () {
+        angular.bootstrap(document, ['embryo.map']);
+    });
 })();
 
