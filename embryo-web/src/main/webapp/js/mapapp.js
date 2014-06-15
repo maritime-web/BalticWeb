f2a4ba75e6ab6944bc8a62ccd1407f2a41888ffd
tejl <jesper@lundogbendsen.dk>
2014-06-15 15:41:35 +0200

    EMBRYO-325, EMBRYO-293, EMBRYO-330: more angular and less event mecanism. EMBRYP-98: metoc images

--- embryo-web/src/main/webapp/js/mapapp.js
+++ embryo-web/src/main/webapp/js/mapapp.js
@@ -1,11 +1,11 @@
 (function() {
     "use strict";
-    angular.module('embryo.map', [ 'ui.bootstrap', 'embryo.menu', 'embryo.routeService', 'embryo.greenpos',
-            'embryo.vessel.controller', 'embryo.ice', 'embryo.msi', 'embryo.routeUpload', 'embryo.schedule',
-            'embryo.routeEdit', 'embryo.decimal', 'embryo.authentication', 'embryo.shape', 'embryo.zoom', 'embryo.metoc', 'embryo.weather.control' ]);
-    
-    angular.element(document).ready(function () {
-        angular.bootstrap(document, ['embryo.map']);
+    angular.module('embryo.map', [ 'ui.bootstrap', 'embryo.menu', 'embryo.yourvessel.control', 'embryo.vessel.control', 'embryo.greenpos',
+            'embryo.vessel.controller', 'embryo.ice', 'embryo.msi.controllers', 'embryo.routeUpload', 'embryo.schedule',
+            'embryo.routeEdit', 'embryo.decimal', 'embryo.authentication', 'embryo.shape', 'embryo.zoom',
+            'embryo.metoc', 'embryo.weather.control', 'embryo.controller.reporting', 'embryo.aisinformation' ]);
+
+    angular.element(document).ready(function() {
+        angular.bootstrap(document, [ 'embryo.map' ]);
     });
 })();
-
