37e0ac15ddbe97415a9e385d57ca37726f163946
Jesper Tejlgaard <j.tejlgaard@gmail.com>
2014-07-31 23:41:09 +0200

    more testing of my new development environment.

--- embryo-common/src/main/java/dk/dma/embryo/common/EmbryonicException.java
+++ embryo-common/src/main/java/dk/dma/embryo/common/EmbryonicException.java
@@ -38,9 +38,5 @@ public class EmbryonicException extends RuntimeException {
     public EmbryonicException(String message, Throwable cause) {
         super(message, cause);
         init();
-
-        int i = 0;
-        i++;
-        System.out.println(i);
     }
 }
