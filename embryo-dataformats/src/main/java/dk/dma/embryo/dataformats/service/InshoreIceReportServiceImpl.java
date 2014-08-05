942b04c8c49f16c04f356a33a84f71b35cc7f3c2
tejl <jesper@lundogbendsen.dk>
2014-08-05 14:12:48 +0200

    EMBRYO-367: GNU -> Apache license

--- embryo-dataformats/src/main/java/dk/dma/embryo/dataformats/service/InshoreIceReportServiceImpl.java
+++ embryo-dataformats/src/main/java/dk/dma/embryo/dataformats/service/InshoreIceReportServiceImpl.java
@@ -1,17 +1,16 @@
-/* Copyright (c) 2011 Danish Maritime Authority
+/* Copyright (c) 2011 Danish Maritime Authority.
  *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 3 of the License, or (at your option) any later version.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * Lesser General Public License for more details.
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * You should have received a copy of the GNU General Public License
- * along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package dk.dma.embryo.dataformats.service;
 
@@ -68,13 +67,6 @@ public class InshoreIceReportServiceImpl implements InshoreIceReportService {
         }
     }
     
-    private class FileComparator implements Comparator<File>{
-        @Override
-        public int compare(File o1, File o2) {
-            return o1.getName().compareTo(o2.getName());
-        }
-    }
-
     private File findInshoreIceReport() throws IOException {
         File dir = new File(localDirectory);
         File[] files = dir.listFiles();
@@ -96,4 +88,12 @@ public class InshoreIceReportServiceImpl implements InshoreIceReportService {
     private void setInshoreIceReport(String provider, InshoreIceReport notifications) {
         inshoreIceReports.put(provider, notifications);
     }
+
+    private class FileComparator implements Comparator<File>{
+        @Override
+        public int compare(File o1, File o2) {
+            return o1.getName().compareTo(o2.getName());
+        }
+    }
+
 }
