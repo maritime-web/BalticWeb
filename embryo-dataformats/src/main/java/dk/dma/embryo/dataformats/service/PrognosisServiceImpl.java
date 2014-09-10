280e338ae70844c008a6d869beff223ababde83b
Jacob Avlund <jacob@avlund.dk>
2014-09-10 13:39:14 +0200

    Test.

--- embryo-dataformats/src/main/java/dk/dma/embryo/dataformats/service/PrognosisServiceImpl.java
+++ embryo-dataformats/src/main/java/dk/dma/embryo/dataformats/service/PrognosisServiceImpl.java
@@ -23,6 +23,9 @@ import javax.annotation.PostConstruct;
 import javax.ejb.Singleton;
 import javax.inject.Inject;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import dk.dma.embryo.dataformats.model.PrognosisType;
 import dk.dma.embryo.dataformats.model.PrognosisType.Type;
 import dk.dma.embryo.dataformats.netcdf.NetCDFResult;
@@ -30,74 +33,79 @@ import dk.dma.embryo.dataformats.netcdf.NetCDFVar;
 
 @Singleton
 public class PrognosisServiceImpl implements PrognosisService {
-    
+
+    private final Logger logger = LoggerFactory.getLogger(PrognosisServiceImpl.class);
+
     private List<PrognosisType> prognosisTypes = new ArrayList<>();
-    
+
     @Inject
     private NetCDFService netCDFService;
-    
+
     @PostConstruct
     public void init() {
         prognosisTypes = createData();
         reParse();
     }
-    
+
     @Override
     public List<PrognosisType> getPrognosisTypes() {
         return prognosisTypes;
     }
-    
+
     @Override
     public PrognosisType getPrognosisType(Type type) {
-        for(PrognosisType t : prognosisTypes) {
-            if(t.getType() == type) {
+        for (PrognosisType t : prognosisTypes) {
+            if (t.getType() == type) {
                 return t;
             }
         }
         return null;
     }
-    
+
     public List<String> getPrognosisList(Type type) {
         Map<String, NetCDFResult> entries = netCDFService.getEntries(getPrognosisType(type));
+        if (entries == null) {
+            logger.info("No NetCDF entries for type " + type + ", re-parsing.");
+            reParse();
+        }
         return new ArrayList<String>(entries.keySet());
     }
-    
+
     @Override
     public NetCDFResult getPrognosis(String id, Type type) {
         return netCDFService.getEntries(getPrognosisType(type)).get(id);
     }
-    
+
     @Override
     public List<String> listAvailableIcePrognoses() {
         return getPrognosisList(Type.ICE_PROGNOSIS);
     }
-    
+
     @Override
     public NetCDFResult getIcePrognosis(String id) {
         return getPrognosis(id, Type.ICE_PROGNOSIS);
     }
-    
+
     @Override
     public List<String> listAvailableWavePrognoses() {
         return getPrognosisList(Type.WAVE_PROGNOSIS);
     }
-    
+
     @Override
     public NetCDFResult getWavePrognosis(String id) {
         return getPrognosis(id, Type.WAVE_PROGNOSIS);
     }
 
-    
     @Override
     public List<String> listAvailableCurrentPrognoses() {
         return getPrognosisList(Type.CURRENT_PROGNOSIS);
     }
-    
+
     @Override
     public NetCDFResult getCurrentPrognosis(String id) {
         return getPrognosis(id, Type.CURRENT_PROGNOSIS);
     }
-    
+
     @Override
     public void reParse() {
         try {
@@ -106,27 +114,27 @@ public class PrognosisServiceImpl implements PrognosisService {
             e.printStackTrace();
         }
     }
-    
+
     public List<PrognosisType> createData() {
-        
+
         List<PrognosisType> types = new ArrayList<>();
-        
+
         // Set up prognosis types
         PrognosisType icePrognosisType = new PrognosisType("Ice prognosis", "ice", Type.ICE_PROGNOSIS);
         Map<String, NetCDFVar> iceVars = icePrognosisType.getVars();
-        
+
         // DMI
         NetCDFVar.addToMap(iceVars, "ice-concentration", "Ice concentration");
         NetCDFVar.addToMap(iceVars, "ice-thickness", "Ice thickness");
         NetCDFVar.addToMap(iceVars, "u-ice", "Ice speed east");
         NetCDFVar.addToMap(iceVars, "v-ice", "Ice speed north");
         NetCDFVar.addToMap(iceVars, "Icing", "Ice accretion risk");
-        
+
         // FCOO
         NetCDFVar.addToMap(iceVars, "ICE", "Ice concentration");
 
         types.add(icePrognosisType);
-        
+
         PrognosisType currentPrognosisType = new PrognosisType("Current prognosis", "current", Type.CURRENT_PROGNOSIS);
         Map<String, NetCDFVar> currentVars = currentPrognosisType.getVars();
 
@@ -135,7 +143,7 @@ public class PrognosisServiceImpl implements PrognosisService {
         NetCDFVar.addToMap(currentVars, "v-current", "Current north");
 
         types.add(currentPrognosisType);
-        
+
         PrognosisType wavePrognosisType = new PrognosisType("Wave prognosis", "wave", Type.WAVE_PROGNOSIS);
         Map<String, NetCDFVar> waveVars = wavePrognosisType.getVars();
 
@@ -150,23 +158,23 @@ public class PrognosisServiceImpl implements PrognosisService {
         NetCDFVar.addToMap(waveVars, "TMN", "Mean wave period");
         NetCDFVar.addToMap(waveVars, "Tz", "Zero upcrossing period");
         NetCDFVar.addToMap(waveVars, "DEPTH", "Water depth");
-        
+
         types.add(wavePrognosisType);
-        
+
         PrognosisType windPrognosisType = new PrognosisType("Wind prognosis", "wind", Type.WIND_PROGNOSIS);
         Map<String, NetCDFVar> windVars = windPrognosisType.getVars();
-        
+
         // DMI
         NetCDFVar.addToMap(windVars, "var245", "Wind speed");
         NetCDFVar.addToMap(windVars, "var249", "Wind direction");
-        
+
         // FCOO
         NetCDFVar.addToMap(windVars, "WU", "Wind speed east");
         NetCDFVar.addToMap(windVars, "WV", "Wind speed north");
-        
+
         types.add(windPrognosisType);
-        
+
         return types;
     }
-    
+
 }
