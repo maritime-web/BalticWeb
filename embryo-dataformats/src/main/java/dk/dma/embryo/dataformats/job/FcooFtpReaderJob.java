35f5fc40df3d848f4894ddf1691a276eceb7da29
Jesper Tejlgaard <j.tejlgaard@gmail.com>
2014-10-29 17:22:26 +0100

    EMBRYO-316: solved compile problem

--- embryo-dataformats/src/main/java/dk/dma/embryo/dataformats/job/FcooFtpReaderJob.java
+++ embryo-dataformats/src/main/java/dk/dma/embryo/dataformats/job/FcooFtpReaderJob.java
@@ -14,20 +14,24 @@
  */
 package dk.dma.embryo.dataformats.job;
 
-import static com.google.common.base.Predicates.not;
-import static dk.dma.embryo.dataformats.job.DmiIceChartPredicates.acceptedIceCharts;
-import static dk.dma.embryo.dataformats.job.DmiIceChartPredicates.validFormat;
-
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.FilenameFilter;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.List;
-import java.util.Map;
-import java.util.Map.Entry;
+import com.google.common.base.Function;
+import com.google.common.collect.Collections2;
+import dk.dma.embryo.common.configuration.Property;
+import dk.dma.embryo.common.configuration.PropertyFileService;
+import dk.dma.embryo.common.log.EmbryoLogService;
+import dk.dma.embryo.common.mail.MailSender;
+import dk.dma.embryo.common.util.NamedtimeStamps;
+import dk.dma.embryo.dataformats.model.ShapeFileMeasurement;
+import dk.dma.embryo.dataformats.persistence.ShapeFileMeasurementDao;
+import org.apache.commons.net.ftp.FTP;
+import org.apache.commons.net.ftp.FTPClient;
+import org.apache.commons.net.ftp.FTPFile;
+import org.apache.commons.net.ftp.FTPFileFilters;
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeZone;
+import org.joda.time.LocalDate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
@@ -39,26 +43,20 @@ import javax.ejb.Timeout;
 import javax.ejb.TimerConfig;
 import javax.ejb.TimerService;
 import javax.inject.Inject;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FilenameFilter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 
-import org.apache.commons.net.ftp.FTP;
-import org.apache.commons.net.ftp.FTPClient;
-import org.apache.commons.net.ftp.FTPFile;
-import org.apache.commons.net.ftp.FTPFileFilters;
-import org.joda.time.DateTime;
-import org.joda.time.DateTimeZone;
-import org.joda.time.LocalDate;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import com.google.common.base.Function;
-import com.google.common.collect.Collections2;
-
-import dk.dma.embryo.common.configuration.Property;
-import dk.dma.embryo.common.configuration.PropertyFileService;
-import dk.dma.embryo.common.log.EmbryoLogService;
-import dk.dma.embryo.common.mail.MailSender;
-import dk.dma.embryo.dataformats.model.ShapeFileMeasurement;
-import dk.dma.embryo.dataformats.persistence.ShapeFileMeasurementDao;
+import static com.google.common.base.Predicates.not;
+import static dk.dma.embryo.dataformats.job.DmiIceChartPredicates.acceptedIceCharts;
+import static dk.dma.embryo.dataformats.job.DmiIceChartPredicates.validFormat;
 
 @Singleton
 @Startup
