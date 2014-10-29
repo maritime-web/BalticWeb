9417efa8b4f79aebb7c8d1270af216076af9e3b2
Jesper Tejlgaard <j.tejlgaard@gmail.com>
2014-10-29 17:28:36 +0100

    merge

--- embryo-web/src/main/webapp/js/mapapp.js
+++ embryo-web/src/main/webapp/js/mapapp.js
@@ -1,17 +1,10 @@
 (function () {
     "use strict";
     angular.module('embryo.map', [ 'ui.bootstrap', 'embryo.menu', 'embryo.yourvessel.control', 'embryo.vessel.control',
-<<<<<<< HEAD
         'embryo.reporting.control', 'embryo.greenpos', 'embryo.vessel.controller', 'embryo.ice.control',
         'embryo.msi.controllers', 'embryo.routeUpload', 'embryo.schedule', 'embryo.routeEdit', 'embryo.decimal',
         'embryo.authentication', 'embryo.shape', 'embryo.zoom', 'embryo.metoc', 'embryo.weather.control',
-        'embryo.controller.reporting', 'embryo.aisinformation', 'embryo.satellite-ice.control' ]);
-=======
-            'embryo.reporting.control', 'embryo.greenpos', 'embryo.vessel.controller', 'embryo.ice.control',
-            'embryo.msi.controllers', 'embryo.routeUpload', 'embryo.schedule', 'embryo.routeEdit', 'embryo.decimal',
-            'embryo.authentication', 'embryo.shape', 'embryo.zoom', 'embryo.metoc', 'embryo.weather.control',
-            'embryo.controller.reporting', 'embryo.aisinformation', 'embryo.forecast.control' ]);
->>>>>>> b2f700898981b571022667fcdf0f599d77cee908
+        'embryo.controller.reporting', 'embryo.aisinformation', 'embryo.forecast.control', 'embryo.satellite-ice.control' ]);
 
     angular.element(document).ready(function () {
         angular.bootstrap(document, [ 'embryo.map' ]);
