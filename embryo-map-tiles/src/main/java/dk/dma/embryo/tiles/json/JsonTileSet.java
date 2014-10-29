c96ac60585b078ff48486ea5637b8667a3940ca6
Jesper Tejlgaard <j.tejlgaard@gmail.com>
2014-10-29 15:56:33 +0100

    EMBRYO-316, EMBRYO-404: Created JAX-RS service to display available tile sets

---
+++ embryo-map-tiles/src/main/java/dk/dma/embryo/tiles/json/JsonTileSet.java
@@ -0,0 +1,98 @@
+/*
+ * Copyright (c) 2011 Danish Maritime Authority.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dk.dma.embryo.tiles.json;
+
+import dk.dma.embryo.tiles.model.BoundingBox;
+import dk.dma.embryo.tiles.model.ImageCenter;
+
+import java.util.Date;
+
+/**
+ * Created by Jesper Tejlgaard on 10/02/14.
+ */
+
+public class JsonTileSet {
+
+    private String name;
+    private String provider;
+    private String source;
+    private String sourceType;
+    private String url;
+    private Date ts;
+    private ImageCenter center;
+    private BoundingBox extend;
+
+    // //////////////////////////////////////////////////////////////////////
+    // Utility methods
+    // //////////////////////////////////////////////////////////////////////
+
+    // //////////////////////////////////////////////////////////////////////
+    // Constructors
+    // //////////////////////////////////////////////////////////////////////
+    public JsonTileSet() {
+    }
+
+    public JsonTileSet(String name, String provider, String source, String sourceType, Date ts, String url) {
+        this.name = name;
+        this.provider = provider;
+        this.source = source;
+        this.sourceType = sourceType;
+        this.ts = ts;
+        this.url = url;
+    }
+
+    public JsonTileSet(String name, String provider, String area, String sourceType, Date ts, String url, ImageCenter center, BoundingBox extend) {
+        this(name, provider, area, sourceType, ts, url);
+        this.center = center;
+        this.extend = extend;
+    }
+
+    // //////////////////////////////////////////////////////////////////////
+    // Property methods
+    // //////////////////////////////////////////////////////////////////////
+    public String getName() {
+        return name;
+    }
+
+    public String getProvider() {
+        return provider;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public String getSourceType() {
+        return sourceType;
+    }
+
+    public Date getTs() {
+        return ts;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public ImageCenter getCenter() {
+        return center;
+    }
+
+    public BoundingBox getExtend() {
+        return extend;
+    }
+}
---
+++ embryo-map-tiles/src/main/java/dk/dma/embryo/tiles/json/TileSetJsonService.java
@@ -0,0 +1,83 @@
+/*
+ * Copyright (c) 2011 Danish Maritime Authority.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package dk.dma.embryo.tiles.json;
+
+import dk.dma.embryo.tiles.model.TileSet;
+import dk.dma.embryo.tiles.service.TileSetDao;
+import org.jboss.resteasy.annotations.GZIP;
+import org.slf4j.Logger;
+
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.CacheControl;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.EntityTag;
+import javax.ws.rs.core.Request;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.ResponseBuilder;
+import java.util.List;
+
+@Path("/tileset")
+public class TileSetJsonService {
+
+    @Inject
+    Logger logger;
+
+    @Inject
+    TileSetDao tileSetDao;
+
+    public TileSetJsonService() {
+        super();
+    }
+
+    private CacheControl getCacheControl() {
+        CacheControl cc = new CacheControl();
+        // 15 minutes
+        //cc.setMaxAge(60*15);
+        cc.setMaxAge(1);
+        cc.setPrivate(false);
+        cc.setNoTransform(false);
+        return cc;
+    }
+
+    @GET
+    @Path("/list/{type}")
+    @Produces("application/json")
+    @GZIP
+    public Response filter(@PathParam("type") String type, @Context Request request) {
+        logger.debug("list()");
+
+        List<TileSet> tileSets = tileSetDao.listByTypeAndStatus(type, TileSet.Status.SUCCESS);
+        List<JsonTileSet> result = TileSet.toJsonModel(tileSets);
+
+        EntityTag tag = EntityTag.valueOf(Integer.toString(result.hashCode()));
+        ResponseBuilder builder = request.evaluatePreconditions(tag);
+
+        // cached resource did change -> serve updated content
+        if (builder == null) {
+            builder = Response.ok(result);
+        }
+        builder.tag(tag);
+        CacheControl cc = getCacheControl();
+        builder.cacheControl(cc);
+        logger.debug("list() : " + result);
+        return builder.build();
+    }
+
+}
