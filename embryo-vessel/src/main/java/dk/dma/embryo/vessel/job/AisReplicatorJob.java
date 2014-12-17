599501043facebd171df5b705e52e2328663b2c3
thomas <thomas@lundogbendsen.dk>
2014-12-17 11:31:14 +0100

    EMBRYO-510 BUG: Tilføj Ole's TimeToLive parametre til AIS REST service

--- embryo-vessel/src/main/java/dk/dma/embryo/vessel/job/AisReplicatorJob.java
+++ embryo-vessel/src/main/java/dk/dma/embryo/vessel/job/AisReplicatorJob.java
@@ -112,8 +112,8 @@ public class AisReplicatorJob {
             logger.info("UPDATE AIS VESSEL DATA INCLUDING NORWEGIAN DATA...");
 
             // Get all vessels from AIS server
-            List<AisViewServiceAllAisData.Vessel> aisServerAllVessels = aisViewWithNorwegianData.vesselList();
-
+            List<AisViewServiceAllAisData.Vessel> aisServerAllVessels = aisViewWithNorwegianData.vesselList(AisViewServiceAllAisData.FILTER_VALUE_PT24H, AisViewServiceAllAisData.FILTER_VALUE_PT24H);
+            
             // Get all vessels from ArcticWeb database
             List<Vessel> articWebVesselsAsList = vesselRepository.getAll(Vessel.class);
 
