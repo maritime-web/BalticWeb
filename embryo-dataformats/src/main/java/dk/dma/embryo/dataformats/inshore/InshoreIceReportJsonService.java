4629835658e942a9a0a70faed2e1771fa13f1624
thomas <thomas@lundogbendsen.dk>
2015-02-17 10:03:18 +0100

    EMBRYO-540 - Make sure all REST services use code 304 for unchanged data. - Denne REST service havde allerede implementeret caching og max age, men nu er den omskrevet til at benytte den generelle version.

--- embryo-dataformats/src/main/java/dk/dma/embryo/dataformats/inshore/InshoreIceReportJsonService.java
+++ embryo-dataformats/src/main/java/dk/dma/embryo/dataformats/inshore/InshoreIceReportJsonService.java
@@ -19,18 +19,17 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.core.CacheControl;
 import javax.ws.rs.core.Context;
-import javax.ws.rs.core.EntityTag;
 import javax.ws.rs.core.Request;
 import javax.ws.rs.core.Response;
-import javax.ws.rs.core.Response.ResponseBuilder;
 
 import org.jboss.resteasy.annotations.GZIP;
 import org.slf4j.Logger;
 
+import dk.dma.embryo.common.json.AbstractRestService;
+
 @Path("/inshore-ice-report")
-public class InshoreIceReportJsonService {
+public class InshoreIceReportJsonService extends AbstractRestService {
 
     @Inject
     private InshoreIceReportService iceInformationService;
@@ -43,43 +42,10 @@ public class InshoreIceReportJsonService {
     @Produces("application/json")
     @GZIP
     public Response inshoreIceReport(@PathParam("provider") String providerKey, @Context Request request) {
-        logger.debug("iceInformations({})", providerKey);
-
-        CacheControl cc = getCacheControl();
+        logger.info("iceInformations({})", providerKey);
 
         InshoreIceReportMerged report = iceInformationService.getInshoreIceReportsMerged(providerKey);
 
-        // sending status code 204
-        if(report == null){
-            ResponseBuilder builder = Response.noContent();
-            builder.cacheControl(cc);
-            return builder.build();
-        }
-        
-        EntityTag etag = new EntityTag(Integer.toString(report.hashCode()));
-        ResponseBuilder builder = request.evaluatePreconditions(etag);
-
-        // cached resource did change -> serve updated content
-        if (builder == null) {
-            builder = Response.ok(report);
-        }
-        builder.cacheControl(cc);
-        builder.tag(etag);
-        
-        Response response = builder.build();
-        return response;
-
+        return super.getResponse(request, report, MAX_AGE_15_MINUTES);
     }
-
-    private CacheControl getCacheControl() {
-        CacheControl cc = new CacheControl();
-        // If resource is younger than max age, then the browser will always use cache version. 
-        // IF resource is older than max age, then a request is sent to the server. 304 may then be returned in case the resource is unmodified.  
-        // 15 minutes chosen because vessels should be able to provoke a refresh, if they know a new report is available 
-        cc.setMaxAge(60 * 15);
-        cc.setPrivate(false);
-        cc.setNoTransform(false);
-        return cc;
-    }
-
 }
