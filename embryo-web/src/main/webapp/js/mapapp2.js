c5030e6592bf083741469d33da9b675de21a7228
Jesper Tejlgaard <j.tejlgaard@gmail.com>
2015-04-23 10:58:43 +0200

    EMBRYO-381: circles brought into separate sar map application

--- embryo-web/src/main/webapp/js/mapapp2.js
+++ embryo-web/src/main/webapp/js/mapapp2.js
@@ -1,6 +1,6 @@
 (function () {
     "use strict";
-    angular.module('embryo.map', ['ui.bootstrap', 'embryo.menu', 'embryo.yourvessel.control', 'embryo.vessel.control',
+    angular.module('embryo.mapapp', ['embryo.map', 'ui.bootstrap', 'embryo.menu', 'embryo.yourvessel.control', 'embryo.vessel.control',
         'embryo.reporting.control', 'embryo.greenpos', 'embryo.vessel.controller', 'embryo.ice.control',
         'embryo.msi.controllers', 'embryo.routeUpload', 'embryo.schedule', 'embryo.routeEdit', 'embryo.decimal',
         'embryo.authentication', 'embryo.shape', 'embryo.zoom', 'embryo.metoc', 'embryo.weather.control',
@@ -8,6 +8,6 @@
         'embryo.areaselect.control', 'embryo.sar.controllers', 'embryo.sar.views']);
 
     angular.element(document).ready(function () {
-        angular.bootstrap(document, ['embryo.map']);
+        angular.bootstrap(document, ['embryo.mapapp']);
     });
 })();
