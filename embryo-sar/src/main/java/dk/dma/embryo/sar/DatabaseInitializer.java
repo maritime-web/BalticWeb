89bbbdd1f88cac1d31050784f47bb84edb2fb23f
Jesper Tejlgaard <j.tejlgaard@gmail.com>
2015-12-11 15:02:35 +0100

    Embryo#24: corrected to comply with checkstyle

--- embryo-sar/src/main/java/dk/dma/embryo/sar/DatabaseInitializer.java
+++ embryo-sar/src/main/java/dk/dma/embryo/sar/DatabaseInitializer.java
@@ -1,3 +1,17 @@
+/* Copyright (c) 2011 Danish Maritime Authority.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package dk.dma.embryo.sar;
 
 import com.n1global.acc.CouchDbConfig;
@@ -134,7 +148,7 @@ public class DatabaseInitializer {
 
     }
 
-    private static final String getUserName(SecuredUser user){
+    private static String getUserName(SecuredUser user) {
         String name = user.getUserName();
         if (user.getRole().getClass() == SailorRole.class) {
             Vessel vessel = ((SailorRole) user.getRole()).getVessel();
@@ -145,7 +159,7 @@ public class DatabaseInitializer {
         return name;
     }
 
-    private static final Integer getMmsi(SecuredUser user){
+    private static Integer getMmsi(SecuredUser user) {
         Integer mmsi = null;
         if (user.getRole().getClass() == SailorRole.class) {
             mmsi = ((SailorRole) user.getRole()).getVessel().getMmsi().intValue();
