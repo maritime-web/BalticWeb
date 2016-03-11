8f4d715962e4094e35b7061199c42b807623d435
Andreas Borchsenius Westh <andreas@lundogbendsen.dk>
2016-03-11 15:38:09 +0100

    MARWEB-37Upgrade to OpenLayers 3: all-at-sea.html contains an OpenLayer 3 prototype with a limited zoom level (you cant zoom out an sea the whole world, only the relevant), zoomslider as requested, collabsable overview map, fullscreen toggle,  natical sea mile scale unit, tiles pre-cached to inifinity, mouse position in {lat,lon}

---
+++ src/main/webapp/js/allatsea/ol3-map.js
@@ -0,0 +1,64 @@
+
+var maritimeweb = {};
+
+maritimeweb.center = new function(){
+    return [22.0, 59.0];
+}
+console.log("loading OL3 map");
+
+var zoomslider = new ol.control.ZoomSlider();
+var seaScaleLine = new ol.control.ScaleLine({
+    units: 'nautical', // 'degrees', 'imperial', 'nautical', 'metric', 'us'
+    minWidth: 80,
+
+});
+var metricScaleLine = new ol.control.ScaleLine({
+    units: 'metric', // 'degrees', 'imperial', 'nautical', 'metric', 'us'
+    minWidth: 80
+
+});
+
+// Add Layers to map-------------------------------------------------------------------------------------------------------
+layer_default_osm = new ol.layer.Tile({
+    source: new ol.source.OSM({layer: 'sat'}),
+    preload: Infinity
+});
+
+
+
+var mousePosition = new ol.control.MousePosition({
+    coordinateFormat: ol.coordinate.createStringXY(4),
+    projection: 'EPSG:4326',
+    target: 'mouseposition',
+    //target: document.getElementById('mouseposition'),
+    undefinedHTML: '&nbsp;'
+});
+var balticExtent = ol.proj.transformExtent([12, 56, 28, 66], 'EPSG:4326', 'EPSG:3857');
+var map = new ol.Map({
+    controls: ol.control.defaults().extend([
+        new ol.control.OverviewMap(),
+        seaScaleLine,
+        // metricScaleLine,
+        zoomslider,
+        new ol.control.FullScreen(),
+        mousePosition
+
+    ]),
+    target: 'map',
+    layers: [
+        layer_default_osm//,
+       /* new ol.layer.Tile({
+            source: new ol.source.XYZ({
+                url: 'http://t1.openseamap.org/seamark/{z}/{x}/{y}.png',
+                crossOrigin: true
+            })
+        })*/
+    ],
+    view: new ol.View({
+        center: ol.proj.fromLonLat(maritimeweb.center),
+        zoom: 7,
+        minZoom: 6,
+        extent: balticExtent
+    })
+});
+console.log("loaded OL3 map center = " + map);
