d8a3838a7f3dd14179f2c3e46a93ae84c13a75c8
Andreas Borchsenius Westh <andreas@lundogbendsen.dk>
2016-05-18 13:19:07 +0200

    #MARWEB-133 remove old arcticweb html and js

--- src/main/webapp/js/control/vessel-actions.js
+++
@@ -1,9 +0,0 @@
-(function() {
-    embryo.vessel.actions = {
-        selectedVessel : function() {
-            return [ "ArcticWeb Reporting", embryo.additionalInformation.route, "Additional Information",
-                    embryo.additionalInformation.historicalTrack, embryo.additionalInformation.nearestShips,
-                    embryo.additionalInformation.distanceCircles ];
-        }
-    };
-})();
