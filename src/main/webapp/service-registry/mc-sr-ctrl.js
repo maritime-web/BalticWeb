f107ef542f30918277f28e944d321af0f1d2283b
Andreas Borchsenius Westh <andreas.westh@gmail.com>
2017-07-11 12:49:55 +0200

    #MARWEB-479 Highlight serivce instance

--- src/main/webapp/service-registry/mc-sr-ctrl.js
+++ src/main/webapp/service-registry/mc-sr-ctrl.js
@@ -8,6 +8,136 @@ angular.module('maritimeweb.app')
             $rootScope.showgraphSidebar = false; // rough disabling of the sidebar
             $scope.highlightedInstance = {};
 
+
+            $scope.mcStylePurple = new ol.style.Style({
+                stroke: new ol.style.Stroke({
+                    color: 'rgba(180, 0, 180, 0.5)',
+                    width: 1
+                }),
+                fill: new ol.style.Fill({
+                    color: 'rgba(180, 0, 180, 0.40)'
+                })
+            });
+            $scope.greenServiceStyle = new ol.style.Style({
+                stroke: new ol.style.Stroke({
+                    color: 'rgba(0, 255, 10, 0.8)',
+                    width: 3
+                }),
+                fill: new ol.style.Fill({
+                    color: 'rgba(5, 200, 10, 0.05)'
+                })
+            });
+
+            $scope.highlightServiceRed = new ol.style.Style({
+                stroke: new ol.style.Stroke({
+                    color: 'rgba(255, 0, 10, 0.5)',
+                    width: 1
+                }),
+                fill: new ol.style.Fill({
+                    color: 'rgba(255, 0, 10, 0.10)'
+                })
+            });
+            /***************************/
+            /** MaritimeCloud Layers      **/
+            /***************************/
+            $scope.createMCLayerGroup = function () {
+
+                // Construct the boundary layers
+                var boundaryLayer = new ol.layer.Vector({
+                    id: 'mcboundary',
+                    //title: 'MaritimeCloud Service Instance AREA',
+                    title: 'mcboundary',
+                    name: 'mcboundary',
+                    zIndex: 11,
+                    source: new ol.source.Vector({
+                        features: new ol.Collection(),
+                        wrapX: false
+                    }),
+                    style: [$scope.greenServiceStyle]
+                });
+
+                var serviceAvailableLayer = new ol.layer.Vector({
+                    id: 'serviceavailboundary',
+                    title: 'Service Available - NO GO AREA',
+                    name: 'Service Available - NO GO AREA',
+                    zIndex: 11,
+                    source: new ol.source.Vector({
+                        features: new ol.Collection(),
+                        wrapX: false
+                    }),
+                    style: [$scope.greenServiceStyle]
+                });
+
+                var duration = 3000;
+                function flash(feature) {
+                    var start = new Date().getTime();
+                    var listenerKey;
+
+                    function animate(event) {
+                        var vectorContext = event.vectorContext;
+                        var frameState = event.frameState;
+                        var flashGeom = feature.getGeometry().clone();
+                        var elapsed = frameState.time - start;
+                        var elapsedRatio = elapsed / duration;
+                        // radius will be 5 at start and 30 at end.
+                        var radius = ol.easing.easeOut(elapsedRatio) * 25 + 5;
+                        var opacity = ol.easing.easeOut(1 - elapsedRatio);
+
+                        var style = new ol.style.Style({
+                            image: new ol.style.Circle({
+                                radius: radius,
+                                snapToPixel: false,
+                                stroke: new ol.style.Stroke({
+                                    color: 'rgba(255, 0, 0, ' + opacity + ')',
+                                    width: 0.25 + opacity
+                                })
+                            })
+                        });
+
+                        vectorContext.setStyle(style);
+                        vectorContext.drawGeometry(flashGeom);
+                        if (elapsed > duration) {
+                            ol.Observable.unByKey(listenerKey);
+                            return;
+                        }
+                        // tell OpenLayers to continue postcompose animation
+                        map.render();
+                    }
+                    listenerKey = map.on('postcompose', animate);
+                }
+
+                serviceAvailableLayer.getSource().on('addfeature', function(e) {
+                    flash(e.feature);
+                });
+
+
+                serviceAvailableLayer.setZIndex(12);
+                serviceAvailableLayer.setVisible(true);
+                serviceAvailableLayer.getSource().clear();
+
+
+                boundaryLayer.setZIndex(11);
+                boundaryLayer.setVisible(true);
+
+
+                /***************************/
+                /** Map creation          **/
+                /***************************/
+
+                // Construct No Go Layer Group layer
+                var mcSRGroupLayer = new ol.layer.Group({
+                    title: 'MC Service Registry',
+                    name: 'MC Service Registry',
+                    zIndex: 11,
+                    layers: [boundaryLayer, serviceAvailableLayer]
+                });
+                mcSRGroupLayer.setZIndex(11);
+                mcSRGroupLayer.setVisible(true);
+
+                return mcSRGroupLayer;
+            }
+
+
             // Cancel any pending NW-NN queries
             var loadTimerService = undefined;
             $scope.$on("$destroy", function () {
@@ -47,7 +177,7 @@ angular.module('maritimeweb.app')
             // Map state and layers
             $scope.mapState = JSON.parse($window.localStorage.getItem('mapState-storage')) ? JSON.parse($window.localStorage.getItem('mapState-storage')) : {};
 
-            $scope.mapMCLayers = MapService.createMCLayerGroup();
+            $scope.mapMCLayers =  $scope.createMCLayerGroup();
             $scope.mapBackgroundLayers = MapService.createStdBgLayerGroup();
 
             // $scope.mapNoGoLayer =  MapService.createNoGoLayerGroup(); // is set in the no-go-layer
@@ -64,34 +194,39 @@ angular.module('maritimeweb.app')
                     $scope.mcServiceRegistryInstances.splice(j, 1);
 
                 }
+                $scope.highlightedInstance = {};
             };
 
 
             $scope.highlightInstance = function (instance) {
+                $scope.highlightedInstance = {};
                 $scope.highlightedInstance.id = instance.id;
                 $scope.highlightedInstance.description = instance.description;
                 $scope.highlightedInstance.name = instance.name;
                 $scope.highlightedInstance.version = instance.version;
                 $scope.highlightedInstance.instanceId = instance.instanceId;
 
-                // TODO Highlight WKT on map
-                /*var feature = $scope.mapMCLayers.getLayers().getArray()[0].getSource().getFeatureById(instance.id);
-                var highlightStyle = new ol.style.Style({
-                    fill: new ol.style.Fill({
-                        color: 'rgba(255, 255, 255, 0.2)'
-                    }),
-                    stroke: new ol.style.Stroke({
-                        color: '#ffcc33',
-                        width: 2
-                    })});
+                var features = $scope.mapMCLayers.getLayers().getArray()[0].getSource().getFeatures();
+                $log.info("Features found " + features.length);
 
-                feature.setStyle(highlightStyle);
+                angular.forEach(features, function(feature) {
+                    $log.info("id= " + feature.getId());
+                    feature.setStyle($scope.greenServiceStyle);
+                });
+                $log.info();
+
+                var feature = $scope.mapMCLayers.getLayers().getArray()[0].getSource().getFeatureById(instance.instanceId);
+
+                $log.info("Feature found by id " + feature.getId());
+
+                feature.setStyle($scope.highlightServiceRed);
                 $scope.mapMCLayers.getLayers().getArray()[0].getSource().addFeature(feature);
-*/
+
             };
 
             $scope.isThereAnyServiceRegistry = function () {
                 $log.info("isThereAnyServiceRegistry");
+                $scope.highlightedInstance = {};
 
                 ServiceRegistryService.getServiceInstances().success(function (services, status) {
 
@@ -116,7 +251,8 @@ angular.module('maritimeweb.app')
                                     // $log.info("wktString=" + wktString);
 
                                     var olFeature = MapService.wktToOlFeature(wktString);
-                                    olFeature.setId(service.id);
+                                    olFeature.id = service.instanceId;
+                                    olFeature.setId(service.instanceId);
                                     olFeature.name = service.name;
                                     olFeature.version = service.version;
                                     olFeature.instanceId = service.instanceId;
@@ -141,26 +277,6 @@ angular.module('maritimeweb.app')
             };
 
 
-            /**************************************/
-            /** Vessel Traffic Service functionality      **/
-            /**************************************/
-
-            $scope.displayServiceInstance = function (size) {
-                growl.info('Display Service Instance');
-                $uibModal.open({
-                    animation: 'true',
-                    templateUrl: 'service-registry/one instance.html',
-                    controller: 'ServiceRegistryInstanceCtrlNotCreated', // TODO: create this controller if needed
-                    size: size
-                })
-            };
-
-
-            // $scope.update = function() {
-            //     $scope.item.size.code = $scope.selectedItem.code
-            //     // use $scope.selectedItem.code and $scope.selectedItem.name here
-            //     // for other stuff ...
-            // }
 
 
             /**
